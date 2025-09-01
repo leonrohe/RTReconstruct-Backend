b05afb101c6d01159dba125e8f894aa6aa315f0b
Leon <LeonRohe.6.5.2003@gmail.com>
2025-09-01 18:12:19 +0200

    Add VisFusion submodule configuration

--- dockerfiles/visfusion.Dockerfile
+++ dockerfiles/visfusion.Dockerfile
@@ -1,16 +1,13 @@
-FROM leonrohe/cuda:10.2-devel-ubuntu18.04
-
-# --- Environment Setup ---
-ENV DEBIAN_FRONTEND=noninteractive
-ENV PYOPENGL_PLATFORM=osmesa
-ENV PATH=/opt/conda/envs/neucon/bin:/opt/conda/bin:$PATH
-ENV CONDA_DEFAULT_ENV=neucon
-ENV MAX_JOBS=2
-ENV MAKEFLAGS="-j2"
+FROM nvidia/cuda:11.3.1-cudnn8-devel-ubuntu18.04
 
 WORKDIR /app
 
 # --- System Dependencies ---
+ENV DEBIAN_FRONTEND=noninteractive
+ENV PYOPENGL_PLATFORM=osmesa
+ENV MAX_JOBS=2
+ENV MAKEFLAGS="-j2"
+
 RUN apt-get update && apt-get install -y \
     wget git cmake build-essential \
     libsparsehash-dev \
@@ -18,24 +15,34 @@ RUN apt-get update && apt-get install -y \
     libegl1-mesa libgl1-mesa-dev \
     libosmesa6-dev libglfw3 libglfw3-dev \
     libgles2-mesa-dev libxrandr-dev libxinerama-dev libxcursor-dev \
-    && rm -rf /var/lib/apt/lists/*
+    python3-distutils \
+    libittnotify0 \     # <-- hier dazu
+ && rm -rf /var/lib/apt/lists/*
 
 # --- Miniconda Installation ---
 RUN wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh && \
     bash /tmp/miniconda.sh -b -p /opt/conda && \
     rm /tmp/miniconda.sh && \
-    conda clean -ya
+    /opt/conda/bin/conda clean -ya
+
+# Make conda available in PATH
+ENV PATH=/opt/conda/bin:$PATH
+
+# Accept Anaconda Terms of Service
+RUN conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main && \
+    conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 
 # --- Copy Conda Environment Files ---
-COPY models/VisFusion/requirements.txt /app/requirements.txt
 COPY models/VisFusion/environment.yml /app/environment.yml
+COPY models/VisFusion/docker_entrypoint.sh /app/docker_entrypoint.sh
+RUN chmod +x /app/docker_entrypoint.sh
 
 # --- Create Conda Environment ---
-RUN conda env create -f /app/environment.yml && \
-    conda clean -ya
+RUN conda env create -f /app/environment.yml
 
-# COPY models/NeuralRecon/docker_entrypoint.sh /app/docker_entrypoint.sh
-# RUN chmod +x /app/docker_entrypoint.sh
+# --- Environment Setup (AFTER env is created) ---
+ENV PATH=/opt/conda/envs/visfusion/bin:/opt/conda/bin:$PATH
+ENV CONDA_DEFAULT_ENV=visfusion
 
 # --- Final Cleanup ---
 RUN conda clean -a -y && rm -rf ~/.cache/pip
--- scripts/visfusion_main.py
+++ scripts/visfusion_main.py
@@ -1,121 +1,23 @@
 import argparse
 import asyncio
 import os
-import time
-import datetime
 import traceback
+from PIL import Image
+import io
 import torch
-import torch.distributed as dist
-from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader
 from torch.utils.data._utils.collate import default_collate
-from tensorboardX import SummaryWriter
-from loguru import logger
 import numpy as np
 
-from models.VisFusion.utils import tensor2float, save_scalars, DictAverageMeter, SaveScene, make_nograd_func
-from models.VisFusion.datasets import transforms, find_dataset_def
-from models.VisFusion.models import VisFusion
-from models.VisFusion.config import cfg, update_config
-from models.VisFusion.datasets.sampler import DistributedSampler
-from models.VisFusion.ops.comm import *
+from utils import SaveScene
+from datasets import transforms
+from models import VisFusion
+from config import cfg, update_config
+from ops.comm import *
+from datasets import transforms
 
-from models.base_model import BaseReconstructionModel
-
-def args():
-    parser = argparse.ArgumentParser(description='A PyTorch Implementation of VisFusion')
-
-    # general
-    parser.add_argument('--cfg',
-                        help='experiment configure file name',
-                        required=True,
-                        type=str)
-
-    parser.add_argument('opts',
-                        help="Modify config options using the command-line",
-                        default=None,
-                        nargs=argparse.REMAINDER)
-    
-    args = parser.parse_args()
-    return args
-
-torch.manual_seed(cfg.SEED)
-torch.cuda.manual_seed(cfg.SEED)
-
-# Augmentation
-n_views = cfg.TEST.N_VIEWS
-random_rotation = False
-random_translation = False
-paddingXY = 0
-paddingZ = 0
-
-transform = [transforms.ResizeImage((640, 480)),
-             transforms.ToTensor(),
-             transforms.RandomTransformSpace(
-                  cfg.MODEL.N_VOX, cfg.MODEL.VOXEL_SIZE, random_rotation, random_translation,
-                  paddingXY, paddingZ, max_epoch=cfg.TRAIN.EPOCHS),
-             transforms.IntrinsicsPoseToProjection(n_views, 4)]
-transforms = transforms.Compose(transform)
-
-Dataset = find_dataset_def(cfg.DATASET)
-test_dataset = Dataset(cfg.TEST.PATH, "test", transforms, cfg.TEST.N_VIEWS, len(cfg.MODEL.THRESHOLDS) - 1, cfg.SCENE)
-TestImgLoader = DataLoader(test_dataset, cfg.BATCH_SIZE, shuffle=False, num_workers=cfg.TEST.N_WORKERS, drop_last=False)
-
-
-def test(from_latest=False):
-    ckpt_list = []
-
-    if cfg.LOADCKPT != '':
-        saved_models = [cfg.LOADCKPT]
-    else:
-        saved_models = [fn for fn in os.listdir(cfg.LOGDIR) if fn.endswith(".ckpt")]
-        saved_models = sorted(saved_models, key=lambda x: int(x.split('_')[-1].split('.')[0]))
-        if from_latest:
-            saved_models = saved_models[-1:]
-
-    for ckpt in saved_models:
-        if ckpt not in ckpt_list:
-            # use the latest checkpoint file
-            loadckpt = os.path.join(cfg.LOGDIR, ckpt)
-            logger.info("resuming " + str(loadckpt))
-            state_dict = torch.load(loadckpt)
-            model.load_state_dict(state_dict['model'], strict=False)
-            epoch_idx = state_dict['epoch']
-
-            TestImgLoader.dataset.tsdf_cashe = {}
-
-            avg_test_scalars = DictAverageMeter()
-            save_mesh_scene = SaveScene(cfg)
-            batch_len = len(TestImgLoader)
-            for batch_idx, sample in enumerate(TestImgLoader):
-                for n in sample['fragment']:
-                    logger.info(n)
-                # save mesh if SAVE_SCENE_MESH and is the last fragment
-                save_scene = cfg.SAVE_SCENE_MESH and batch_idx == batch_len - 1
-
-                start_time = time.time()
-                loss, scalar_outputs, outputs = test_sample(sample, save_scene)
-                logger.info('Epoch {}, Iter {}/{}, test loss = {:.3f}, time = {:3f}'.format(epoch_idx, batch_idx,
-                                                            len(TestImgLoader), loss, time.time() - start_time))
-                avg_test_scalars.update(scalar_outputs)
-                del scalar_outputs
-
-                if batch_idx % 100 == 0:
-                    logger.info("Iter {}/{}, test results = {}".format(batch_idx, len(TestImgLoader),
-                                                                       avg_test_scalars.mean()))
-
-                # save mesh
-                if cfg.SAVE_SCENE_MESH:
-                    save_mesh_scene(outputs, sample, epoch_idx)
-            ckpt_list.append(ckpt)
-
-@make_nograd_func
-def test_sample(sample, save_scene=False):
-    model.eval()
-    outputs, loss_dict = model(sample, save_scene)
-    loss = loss_dict['total_loss']
-
-    return tensor2float(loss), tensor2float(loss_dict), outputs
+from common_utils.myutils import ModelResult
+from recon_models.base_model import BaseReconstructionModel
 
 class VisFusionReconstructionModel(BaseReconstructionModel):
     def __init__(self, model_name, server_url = "ws://localhost:5000/ws/model"):
@@ -134,13 +36,12 @@ class VisFusionReconstructionModel(BaseReconstructionModel):
             item = {
                 'imgs': visfusion_fragment["images"],
                 'intrinsics': np.stack(visfusion_fragment['intrinsics']),
-                'extrinsics': np.stack(visfusion_fragment['extrinsics']),
+                'poses': np.stack(visfusion_fragment['extrinsics']),
                 'scene': scene_name,
                 'fragment': fragment_name,
                 'epoch': [None],
                 'vol_origin': np.array([0, 0, 0])
             }
-
             item = TRANSFORMS(item)
             item = default_collate([item])
         except Exception as e:
@@ -153,38 +54,165 @@ class VisFusionReconstructionModel(BaseReconstructionModel):
             with torch.no_grad():
                 MODEL.eval()
                 outputs, loss_dict  = MODEL(item, True)
+                print("[INFO] Inference complete.")
+
+                if outputs == {}:
+                    print("[ERROR] No output from the model. Resetting ...")
+                    MODEL = VisFusion(cfg).cuda().eval()
+                    MODEL = torch.nn.DataParallel(MODEL, device_ids=[0])
+                    MODEL.load_state_dict(STATE_DICT['model'], strict=False)
+                    await self.send_result(None, False)
+                    return
+
+                tsdf = outputs['scene_tsdf'][0].data.cpu().numpy()
+                origin = outputs['origin'][0].data.cpu().numpy()
+                origin[2] -= 1.5
+
+                mesh = SaveScene.tsdf2mesh(cfg.MODEL.VOXEL_SIZE, origin, tsdf, cfg.MODEL.PASS_LAYERS, cfg.MODEL.SINGLE_LAYER_MESH)
+                glb_bytes = mesh.export(file_type='glb')
+                result: ModelResult = ModelResult(scene_name, glb_bytes, False)
+                result.SetRotationFromEuler(90, 0, 180, degrees=True)
+
+                await self.send_result(result)
+
+                self.fragmentIdx += 1
                 
 
         except Exception as e:
             print(f"[ERROR] Inference failed, because {e}.")
             print(traceback.format_exc())
 
-    def transform_base_fragment(self, fragment: dict):
-        pass
+    def transform_base_fragment(self, fragment: dict) -> dict:
+        from transforms3d.quaternions import quat2mat
 
-MODEL_NAME = os.getenv("MODEL_NAME", "neucon")
+        def transform_image(image: bytes):
+            image = Image.open(io.BytesIO(image)).convert("RGB")
+            return image
+        
+        def transform_pose(pose: dict, use_homogenous=True):
+            from transforms3d.quaternions import quat2mat
+
+            def rotx(t):
+                ''' 3D Rotation about the x-axis. '''
+                c = np.cos(t)
+                s = np.sin(t)
+                return np.array([[1, 0, 0],
+                                [0, c, -s],
+                                [0, s, c]])
+
+            trans = pose['camera_position']
+            quat = pose['camera_rotation']
+
+            trans[-1] = -trans[-1]
+            quat[0] = -quat[0]
+            quat[1] = -quat[1]
+
+
+            rot_mat = quat2mat(np.append(quat[-1], quat[:3]).tolist())
+            rot_mat = rot_mat.dot(np.array([
+                [1, 0, 0],
+                [0, -1, 0],
+                [0, 0, -1]
+            ]))
+            rot_mat = rotx(np.pi / 2) @ rot_mat
+            trans = rotx(np.pi / 2) @ trans
+
+            trans_mat = np.zeros([3, 4])
+            trans_mat[:3, :3] = rot_mat
+            trans_mat[:3, 3] = trans
+            if use_homogenous:
+                trans_mat = np.vstack((trans_mat, [0, 0, 0, 1]))
+            
+            # Idek why this is needed, but it is in the original code
+            # Supposedly to match the training data
+            # lets just hardcode this value, yayyy
+            # Fun Fact: This took me the whole day to figure out :))
+            trans_mat[2, 3] += 1.5 
+
+            return trans_mat
+
+        # may need to scale them based on original image size late on
+        def transform_intrinsics(intrinsics: dict):
+            fx, fy = intrinsics['focal_length']
+            cx, cy = intrinsics['principal_point']
+
+            K = np.array([
+                [fx, 0, cx],
+                [0, fy, cy],
+                [0,  0,  1]
+            ], dtype=np.float32)
+
+            return K
+
+        return {
+            "images": [transform_image(image) for image in fragment["images"]],
+            "extrinsics": [transform_pose(pose) for pose in fragment["extrinsics"]],
+            "intrinsics": [transform_intrinsics(intrinsic) for intrinsic in fragment["intrinsics"]]
+        }
+
+# Augmentation (Test only)
+n_views = cfg.TEST.N_VIEWS
+random_rotation = False
+random_translation = False
+paddingXY = 0
+paddingZ = 0
+
+TRANSFORMS = [transforms.ResizeImage((640, 480)),
+             transforms.ToTensor(),
+             transforms.RandomTransformSpace(
+                  cfg.MODEL.N_VOX, cfg.MODEL.VOXEL_SIZE, random_rotation, random_translation,
+                  paddingXY, paddingZ, max_epoch=cfg.TRAIN.EPOCHS),
+             transforms.IntrinsicsPoseToProjection(n_views, 4)]
+TRANSFORMS = transforms.Compose(TRANSFORMS)
+
+def args():
+    parser = argparse.ArgumentParser(description='A PyTorch Implementation of VisFusion')
+
+    # general
+    parser.add_argument('--cfg',
+                        help='experiment configure file name',
+                        required=True,
+                        type=str)
+
+    parser.add_argument('opts',
+                        help="Modify config options using the command-line",
+                        default=None,
+                        nargs=argparse.REMAINDER)
+    
+    args = parser.parse_args()
+    return args
+
+MODEL_NAME = os.getenv("MODEL_NAME", "visfusion")
 SERVER_URL = os.getenv("SERVER_URL", "ws://router:5000/ws/model")
 if __name__ == "__main__":
+    # --- cfg setup ---
     args = args()
     update_config(cfg, args)
-
-    cfg.defrost()
-    cfg.LOCAL_RANK = args.local_rank
     cfg.freeze()
 
+    # --- torch seed ---
+    torch.manual_seed(cfg.SEED)
+    torch.cuda.manual_seed(cfg.SEED)
+
+    # --- (non dist) model setup ---
     print("[INFO] Initializing the model on the GPU...")
     MODEL = VisFusion(cfg)
     MODEL = torch.nn.DataParallel(MODEL, device_ids=[0])
     MODEL.cuda()
 
-    saved_models = [fn for fn in os.listdir(cfg.LOGDIR) if fn.endswith(".ckpt")]
-    saved_models = sorted(saved_models, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+    # --- load model checkpoint ---
+    if cfg.LOADCKPT != "":
+        saved_models = [cfg.LOADCKPT]
+    else:
+        saved_models = [fn for fn in os.listdir(cfg.LOGDIR) if fn.endswith(".ckpt")]
+        saved_models = sorted(saved_models, key=lambda x: int(x.split('_')[-1].split('.')[0]))
     chkpt = os.path.join(cfg.LOGDIR, saved_models[-1])
 
     print(f"[INFO] Resuming from {chkpt}")
-    state_dict = torch.load(chkpt)
-    MODEL.load_state_dict(state_dict['model'], strict=False)
+    STATE_DICT = torch.load(chkpt)
+    MODEL.load_state_dict(STATE_DICT['model'], strict=False)
 
+    # --- start recon model ---
     recon_model = VisFusionReconstructionModel(MODEL_NAME, SERVER_URL)
     asyncio.run(recon_model.connect())
 
